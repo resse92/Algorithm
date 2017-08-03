96679bf5731824cc46b7c607e51fdd623865db5f
zhuruliang <zrl920825@hotmail.com>
2017-08-03 19:07:16 +0800

    learn dp with lis

--- LeetCode_swift.playground/Contents.swift
+++ LeetCode_swift.playground/Contents.swift
@@ -109,7 +109,102 @@ var str = "Hello, playground"
 ///<-----------------------------------------------------
 
 
+///---------------------------------------------------->
+/// this is no leetcode question
+/// learn dp with lis
 
+class Lis1 {
+    var lisArray:[Int] = []
+    let primitiveArray = [2, 1, 6, 3, 5, 4, 8, 7, 9]
+    
+    init() {
+        print(lis())
+    }
+    
+    func lis() -> Int {
+        for a in primitiveArray {
+            lisArray.append(1)
+            let index = primitiveArray.index(of: a)!
+            for b in primitiveArray[0...index] {
+                let indexB = primitiveArray.index(of: b)!
+                // print("a=" + String(a))
+                // print("b=" + String(b))
+                // print(lisArray)
+                // if b < a && lisArray[indexB] + 1 > lisArray[index]
+                if a > b {
+                    lisArray[index] = lisArray[indexB] + 1 > lisArray[index] ? lisArray[indexB]+1 : lisArray[index]
+                }
+            }
+        }
+        var max = 0
+        for x in lisArray {
+            if x > max {
+                max = x
+            }
+        }
+        return max
+    }
+    
+}
+//Lis1()
+// 优化后O(nlogn)
+class Lis2 {
+    var lisArray:[Int] = []
+    let primitiveArray = [2, 1, 6, 3, 5, 4, 8, 7, 9]
+    var lisLen = 0
+    
+    init() {
+        lis()
+        print(111)
+        print(lisArray)
+    }
+    
+    func lis() -> Int {
+        lisLen = 1
+        lisArray.append(primitiveArray[0])
+        
+        for k in primitiveArray[1...primitiveArray.count-1] {
+//            let index = primitiveArray.index(of: k)!
+            print(lisLen)
+            print(lisArray)
+            if k > lisArray[lisLen - 1] { // k是否大于lisArray的最大元素
+                lisArray.append(k)
+                lisLen += 1
+            } else {
+                let pos = biSearch(lisArray, lisLen, k)
+                print("pos=" + String(pos))
+                if pos >= lisArray.count {
+                    lisArray.append(k)
+                } else {
+                    lisArray[pos] = k
+                }
+            }
+        }
+        
+        return 1
+    }
+    
+    func biSearch(_ b: [Int], _ len: Int, _ w: Int) -> Int {
+        var left = 0
+        var right = len - 1
+        print(left + (right - left) / 2)
+        while left <= right {
+            let mid = left + (right - left) / 2
+            if lisArray[mid] > w {
+                right = mid - 1
+            } else if lisArray[mid] < w {
+                left = mid + 1
+            } else {
+                return mid
+            }
+        }
+        
+        return left // 数组中不存在该元素，则返回应该插入的位置
+    }
+}
+Lis2()
+
+///<---------------------------------------------------
 
 class Test {
     
