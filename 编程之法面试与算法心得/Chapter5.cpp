cef28956bf1df65a1638d91f15fb7ac7ac7ae4b4
AlexZHU <zrl920825@hotmail.com>
2017-08-07 22:29:38 +0800

    chapter 5.4 learning

--- 编程之法面试与算法心得/Chapter5.cpp
+++ 编程之法面试与算法心得/Chapter5.cpp
@@ -138,3 +138,36 @@ int MinPathSum(int a[N][N], int n)
     }
     return dp[P][n - 1][n - 1];
 }
+
+// 5.4 交替字符串
+// 输入三个字符串,判断第三个字符串是否由前两个字符串交替而成
+
+public boolean IsInterleave(String s1, String 2, String 3){
+        int n = s1.length(), m = s2.length(), s = s3.length();
+
+        //如果长度不一致，则s3不可能由s1和s2交错组成
+        if (n + m != s)
+                return false;
+
+        boolean[][]dp = new boolean[n + 1][m + 1];
+
+        //在初始化边界时，我们认为空串可以由空串组成，因此dp[0][0]赋值为true。
+        dp[0][0] = true;
+
+        for (int i = 0; i < n + 1; i++){
+                for (int j = 0; j < m + 1; j++){
+                        if ( dp[i][j] || (i - 1 >= 0 && dp[i - 1][j] == true &&
+                                //取s1字符
+                                s1.charAT(i - 1) == s3.charAT(i + j - 1)) ||
+
+                                (j - 1 >= 0 && dp[i][j - 1] == true &&
+                                //取s2字符
+                                s2.charAT(j - 1) == s3.charAT(i + j - 1)) )
+
+                                dp[i][j] = true;
+                        else
+                                dp[i][j] = false;
+                }
+        }
+        return dp[n][m]
+}
