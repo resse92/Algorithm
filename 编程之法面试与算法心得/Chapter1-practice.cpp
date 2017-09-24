caa02231f813e31fbcf9b3f59a5f9e078ae5655a
zhuruliang <zrl920825@hotmail.com>
2017-09-24 22:56:37 +0800

    practice 1.2

--- 编程之法面试与算法心得/Chapter1-practice.cpp
+++ 编程之法面试与算法心得/Chapter1-practice.cpp
@@ -30,4 +30,45 @@ char FirstNotRepeatingChar(char* pString)
       // if the string is empty 
       // or every char in the string appears at least twice
       return 0;
-} 
+}
+
+// 2 对称子字符串的最大长度
+
+// Whether a string between pBegin and pEnd is symmetrical?
+bool IsSymmetrical(char* pBegin, char* pEnd)
+{
+       if(pBegin == NULL || pEnd == NULL || pBegin > pEnd)
+              return false;
+ 
+       while(pBegin < pEnd)
+       {
+              if(*pBegin != *pEnd)
+                     return false;
+ 
+              pBegin++;
+              pEnd --;
+       }
+       return true;
+}
+// 使用manacher算法O(n) http://blog.csdn.net/asdfghjkl1993/article/details/28605513。
+int Manacher(int n)  
+{  
+    int mx=0;  
+    int id;  
+    p[0]=0;  
+    for(int i=1;i<n;i++)  
+    {  
+        p[i]=1;  
+        if(mx>i)  
+        {  
+            p[i]=min(p[2*id-i],mx-i);  
+        }  
+        while(str[i-p[i]]==str[i+p[i]]) p[i]++;  
+        if(i+p[i]>mx)  
+        {  
+            id=i;  
+            mx=i+p[i];  
+        }  
+    }  
+    return mx;
+}  
